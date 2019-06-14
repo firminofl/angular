52af6d0b9ded1fb90058a37831a6582c3ad11b2a
firminofl <30608181+firminofl@users.noreply.github.com>
2019-06-14 15:24:43 -0300

    Aplicação Meat-App

---
+++ src/app/restaurants/restaurants.service.ts
@@ -0,0 +1,43 @@
+import { Restaurant } from "./restaurant/restaurant.model";
+
+import {MEAT_API} from '../app.api'
+
+import { Injectable } from '@angular/core'
+
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/operator/map'
+
+import { Http } from '@angular/http'
+import { Observable } from "rxjs/Observable";
+import { ErrorHandler } from "app/app.error-handler";
+import { MenuItem } from "app/restaurant-detail/menu-item/menu-item.model";
+
+@Injectable()
+export class RestaurantService {
+    
+    constructor(private http: Http){}
+
+    restaurants(): Observable<Restaurant[]> {
+        return this.http.get(`${MEAT_API}/restaurants`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handler)
+    }
+
+    restaurantById(id: string): Observable<Restaurant> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handler)
+    }
+
+    reviewsOfRestaurant(id: string): Observable<any> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handler)
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handler)
+    }
+}
